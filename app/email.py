8ab1d7190b11a7396c5a290fb00bec8f9bb2707d
becky nayere <rebecca.nayere@gmail.com>
2020-09-26 11:11:09 +0300

    add files and folders to templates

--- app/email.py
+++ app/email.py
@@ -3,7 +3,7 @@ from flask import render_template
 from . import mail
 
 def mail_message(subject,template,to,**kwargs):
-    sender_email = 'pitchworld4@gmail.com'
+    sender_email = 'nereahhopebecky@gmail.com@gmail.com'
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
     email.html = render_template(template + ".html",**kwargs)
